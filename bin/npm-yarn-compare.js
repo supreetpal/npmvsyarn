90cd36680d5007074995403da9c8f21c645e8c53
Supreet Pal Singh <supreetpal@gmail.com>
2016-10-15 21:26:58 +0530

    Move from child_process to shelljs. Store each time in separate variables.

--- bin/npm-yarn-compare.js
+++ bin/npm-yarn-compare.js
@@ -2,43 +2,43 @@
 
 process.title = 'npm-yarn-compare';
 
-let child_process = require('child_process'),
-    prettyMs = require('pretty-ms'),
-    clc = require('cli-color');
+let prettyMs = require('pretty-ms'),
+    clc = require('cli-color'),
+    sh = require('shelljs');
 
 let packageName = process.argv[2];
 
 console.log('🛀 Checking if yarn is installed...');
-let cmdExist = child_process.execSync('hash yarn').toString().trim();
+let cmdExist = sh.exec('hash yarn', {silent:true}).output;
 
 if (cmdExist !== '') {
     console.log('🤕 yarn command not found in a global scope.');
     console.log('😴 Installing yarn...');
-    child_process.execSync('npm install -g yarn');
+    sh.exec('npm install -g yarn', {silent:true});
 } else {
     console.log('👯 yarn exists in a global scope');
 }
 
 console.log('🛀 Adding ' + clc.green(packageName) + ' with ' + clc.blue('yarn'));
 let timeStamp = Date.now();
-child_process.execSync('yarn add ' + packageName + ' > /dev/null 2>&1');
-let timeTaken = Date.now() - timeStamp;
-console.log('🚀 Time taken by yarn to add ' + clc.red(prettyMs(timeTaken)) + '');
+sh.exec('yarn add ' + packageName, {silent:true});
+let yarnAddTime = Date.now() - timeStamp;
+console.log('🚀 Time taken by yarn to add ' + clc.red(prettyMs(yarnAddTime)) + '');
 
 console.log('🛀 Removing ' + clc.green(packageName) + ' with ' + clc.blue('yarn'));
 timeStamp = Date.now();
-child_process.execSync('yarn remove ' + packageName + ' > /dev/null 2>&1');
-timeTaken = Date.now() - timeStamp;
-console.log('🚀 Time taken by yarn to remove ' + clc.red(prettyMs(timeTaken)) + '');
+sh.exec('yarn remove ' + packageName, {silent:true});
+let yarnRemoveTime = Date.now() - timeStamp;
+console.log('🚀 Time taken by yarn to remove ' + clc.red(prettyMs(yarnRemoveTime)) + '');
 
 timeStamp = Date.now();
 console.log('🛀 Installing ' + clc.green(packageName) + ' with ' + clc.blue('npm'));
-child_process.execSync('npm install ' + packageName + ' > /dev/null 2>&1');
-timeTaken = Date.now() - timeStamp;
-console.log('🚀 Time taken by npm to install ' + clc.red(prettyMs(timeTaken)) + '');
+sh.exec('npm install ' + packageName, {silent:true});
+let npmAddTime = Date.now() - timeStamp;
+console.log('🚀 Time taken by npm to install ' + clc.red(prettyMs(npmAddTime)) + '');
 
 console.log('🛀 Removing ' + clc.green(packageName) + ' with ' + clc.blue('npm'));
 timeStamp = Date.now();
-child_process.execSync('npm uninstall ' + packageName + ' > /dev/null 2>&1');
-timeTaken = Date.now() - timeStamp;
-console.log('🚀 Time taken by npm to uninstall ' + clc.red(prettyMs(timeTaken)) + '');
+sh.exec('npm uninstall ' + packageName, {silent:true});
+let npmRemoveTime = Date.now() - timeStamp;
+console.log('🚀 Time taken by npm to uninstall ' + clc.red(prettyMs(npmRemoveTime)) + '');
