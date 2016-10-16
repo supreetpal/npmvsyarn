e151a4137594c3b203d7dc394dca7597e9ede909
Supreet Pal Singh <supreetpal@gmail.com>
2016-10-16 20:07:24 +0530

    Add some comments.

--- bin/npmvsyarn.js
+++ bin/npmvsyarn.js
@@ -8,6 +8,7 @@ let prettyMs = require('pretty-ms'),
 
 let packageName = process.argv[2];
 
+//Check if yarn exists, if not install it
 console.log('🕵 Checking if ' + clc.blue('yarn') + ' is installed...');
 
 if (!sh.which('yarn')) {
@@ -18,24 +19,28 @@ if (!sh.which('yarn')) {
     console.log('👯 ' + clc.blue('yarn') + ' exists in a global scope');
 }
 
+//Install module using yarn add
 console.log('🛀 Adding ' + clc.green(packageName) + ' with ' + clc.blue('yarn'));
 let timeStamp = Date.now();
 sh.exec('yarn add ' + packageName, {silent:true});
 let yarnAddTime = Date.now() - timeStamp;
 console.log('🚀 Time taken by yarn to add ' + clc.red(prettyMs(yarnAddTime)) + '');
 
+//Uninstall module using yarn remove
 console.log('🛀 Removing ' + clc.green(packageName) + ' with ' + clc.blue('yarn'));
 timeStamp = Date.now();
 sh.exec('yarn remove ' + packageName, {silent:true});
 let yarnRemoveTime = Date.now() - timeStamp;
 console.log('🚀 Time taken by yarn to remove ' + clc.red(prettyMs(yarnRemoveTime)) + '');
 
+//Install module using npm install
 timeStamp = Date.now();
 console.log('🛀 Installing ' + clc.green(packageName) + ' with ' + clc.blue('npm'));
 sh.exec('npm install ' + packageName, {silent:true});
 let npmAddTime = Date.now() - timeStamp;
 console.log('🚀 Time taken by npm to install ' + clc.red(prettyMs(npmAddTime)) + '');
 
+//Uninstall module using npm uninstall
 console.log('🛀 Removing ' + clc.green(packageName) + ' with ' + clc.blue('npm'));
 timeStamp = Date.now();
 sh.exec('npm uninstall ' + packageName, {silent:true});
